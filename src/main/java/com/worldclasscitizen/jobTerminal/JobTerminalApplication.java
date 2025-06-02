9aa3367935911d16815b2bfa90332362e48eeac1
teddynu <jk62362@mensakorea.org>
2025-06-03 05:05:19 +0900

    Initial commit - application.properties 에 Jsoup, Selenium, H2 Database 의존성 추가

---
+++ src/main/java/com/worldclasscitizen/jobTerminal/JobTerminalApplication.java
@@ -0,0 +1,13 @@
+package com.worldclasscitizen.jobTerminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobTerminalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobTerminalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/worldclasscitizen/jobTerminal/JobTerminalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldclasscitizen.jobTerminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTerminalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
