eca429efe25a3462daa0fd05032a182f6b1c6e6d
Jinwoo Choi <jk62362@mensakorea.org>
2025-06-03 17:20:49 +0900

    feat : 필터링이 적용된 채용 공고 링크 수집 기능 구현 - 직무 -> 개발 -> 백엔드 개발자만 필터링 - 근무 경력 -> 신입만 필터링

---
+++ src/main/java/com/worldclasscitizen/jobTerminal/sites/wanted/WantedCrawler.java
@@ -0,0 +1,39 @@
+package com.worldclasscitizen.jobTerminal.sites.wanted;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.ArrayList;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WantedCrawler {
+
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        WebDriver driver = new ChromeDriver(options);
+
+        // 518 -> 개발 직무 전체, 660 -> 자바 개발자만 필터링, years=0 -> 신입만 필터링
+        String url = "https://www.wanted.co.kr/wdlist/518/660?years=0";
+        driver.get(url);
+
+        try {
+            Thread.sleep(2000); // 페이지 로딩 대기
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            System.out.println("페이지 로딩 중 Thread 가 Interrupt 되었습니다. " + e.getMessage());
+        }
+
+        List<WebElement> jobCards = driver.findElements(By.cssSelector("div[data-cy='job-card']")); // 실제 클래스명 확인 필요
+        List<String> jobLinks = new ArrayList<>();
+        for (WebElement card : jobCards) {
+            WebElement anchor = card.findElement(By.tagName("a"));
+            String link = "https://www.wanted.co.kr" + anchor.getAttribute("href");
+            jobLinks.add(link);
+        }
+    }
+}
